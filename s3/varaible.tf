9df2a64e32f26473cefdd9bb132647be3ae5518a
Aditya K <76086931+akoneru121@users.noreply.github.com>
2024-10-30 11:21:28 -0500

    Update varaible.tf

--- s3/varaible.tf
+++ s3/varaible.tf
@@ -1,2 +1,22 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
 
+variable "versioning" {
+  description = "Enable versioning on the bucket"
+  type        = bool
+  default     = true
+}
+
+variable "lifecycle_policy" {
+  description = "JSON configuration for lifecycle policy rules"
+  type        = list(object({
+    id      = string
+    enabled = bool
+    transition_days = number
+    storage_class   = string
+  }))
+  default = []
+}
 
