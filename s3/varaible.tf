69ecc9cb545c78afa9ce7e72b47810d3b14518a2
Aditya K <76086931+akoneru121@users.noreply.github.com>
2024-10-30 11:20:08 -0500

    Update varaible.tf

--- s3/varaible.tf
+++ s3/varaible.tf
@@ -1,11 +1,2 @@
-# modules/s3_bucket/outputs.tf
 
-output "bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.this.bucket
-}
 
-output "bucket_arn" {
-  description = "The ARN of the S3 bucket"
-  value       = aws_s3_bucket.this.arn
-}
