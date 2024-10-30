8347aa5796400aa5249197bba1a9dedea7405c06
Aditya K <76086931+akoneru121@users.noreply.github.com>
2024-10-30 11:15:02 -0500

    Update security.tf

--- us-west-2/security.tf
+++ us-west-2/security.tf
@@ -5,14 +5,14 @@ resource "aws_security_group" "tomcat_sg_1" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Adjust this based on your security requirements
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Adjust this based on your security requirements
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
   ingress {
